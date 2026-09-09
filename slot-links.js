18e45b7ff8ebaed75279a1d509ab4942256ca0ca
Martin Kaloev <kaloev_92@mail.ru>
2026-09-09 12:56:11 +0300

    Hmmm, it spinss live update on videos with updated links in 9 september , end for day cycle
    Cache-buster: v19

--- slot-links.js
+++ slot-links.js
@@ -4,7 +4,39 @@
 // by the browser over file://, but a script tag loading the same file
 // does not. Edited by add-video.sh; you shouldn't need to hand-edit this.
 window.SLOT_LINKS = [
-  { "url": "https://www.youtube.com/watch?v=IkMxln99szo", "label": "Wellcome video", "week": "Week 37", "date": "2026-09-09", "version": 1 },
-  { "url": "https://www.youtube.com/watch?v=8Z7NXMryPz4", "label": "Week 3 update", "week": "Week 37", "date": "2026-09-09", "version": 2 },
-  { "url": "https://www.youtube.com/shorts/utMh43w6f0g", "label": "bitesize update week 3", "week": "Week 37", "date": "2026-09-09", "version": 3 }
+  {
+    "url": "https://www.youtube.com/watch?v=IkMxln99szo",
+    "label": "Wellcome video",
+    "week": "Week 37",
+    "date": "2026-09-09",
+    "version": 1
+  },
+  {
+    "url": "https://www.youtube.com/watch?v=8Z7NXMryPz4",
+    "label": "Week 3 update",
+    "week": "Week 37",
+    "date": "2026-09-09",
+    "version": 2
+  },
+  {
+    "url": "https://www.youtube.com/shorts/utMh43w6f0g",
+    "label": "bitesize update week 3",
+    "week": "Week 37",
+    "date": "2026-09-09",
+    "version": 3
+  },
+  {
+    "url": "https://www.youtube.com/watch?v=8zBfoRwDTAU",
+    "label": "My new DRL proejct",
+    "week": "Week 37",
+    "date": "2026-09-09",
+    "version": 4
+  },
+  {
+    "url": "https://www.youtube.com/shorts/E_Srr9EO_og",
+    "label": "Hm, it spins",
+    "week": "Week 37",
+    "date": "2026-09-09",
+    "version": 5
+  }
 ];
